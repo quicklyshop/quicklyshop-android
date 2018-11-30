7c97e8976696093d5183bd8fd5288e93d3ced571
Alejandro Anzola <anzola95avila@gmail.com>
2018-11-30 00:54:47 -0500

    commit inicial: plantilla inicial

---
+++ app/src/main/java/com/cosw/quicklyshop/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.cosw.quicklyshop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.cosw.quicklyshop.view.ContainerActivity;
+import com.cosw.quicklyshop.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+    public void goHome(View view){
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/cosw/quicklyshop/view/PictureDetailActivity.java
@@ -0,0 +1,36 @@
+package com.cosw.quicklyshop.view;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
+
+import com.cosw.quicklyshop.R;
+
+public class PictureDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_detail);
+        showToolbar("", true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            getWindow().setEnterTransition(new Fade());
+        }
+
+    }
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+
+    }
+
+}
