de19399c0dd27ffa6d454e35bc8ca8b6a7747613
CamilaGomez <camilagomezr05@gmail.com>
2018-12-04 00:15:34 -0500

    Vista de factura

--- app/src/main/java/com/cosw/quicklyshop/view/FacturaActivity.java
+++ app/src/main/java/com/cosw/quicklyshop/view/FacturaActivity.java
@@ -1,4 +1,25 @@
 package com.cosw.quicklyshop.view;
 
-public class FacturaActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.cosw.quicklyshop.R;
+
+public class FacturaActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factura);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_factura), true);
+
+    }
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
 }
+
