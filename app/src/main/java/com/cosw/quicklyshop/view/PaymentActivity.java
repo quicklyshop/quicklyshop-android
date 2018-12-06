d3248e43aa62a91f8cd0d7f60399b1551c1471ae
Alejandro Anzola <anzola95avila@gmail.com>
2018-12-06 17:02:15 -0500

    establece flujo de pago al home

--- app/src/main/java/com/cosw/quicklyshop/view/PaymentActivity.java
+++ app/src/main/java/com/cosw/quicklyshop/view/PaymentActivity.java
@@ -1,8 +1,12 @@
 package com.cosw.quicklyshop.view;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 import com.cosw.quicklyshop.R;
 
@@ -21,6 +25,22 @@ public class PaymentActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(tittle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
 
+    public void payAndGoToHome(View view) {
+        Toast.makeText(getApplicationContext(), "Se esta procesando su solicitud", Toast.LENGTH_LONG).show();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), "Se ha realizado el pago satisfactoriamente", Toast.LENGTH_LONG).show();
+            }
+
+        }, 5000); // 5000ms delay
+
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
     }
 }
